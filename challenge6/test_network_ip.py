bb693950a9982602c6b741595315cfd5cf076f28
loudsquelch <35978618+loudsquelch@users.noreply.github.com>
2019-07-25 08:27:22 +0100

    Update test_network_ip.py

--- challenge6/test_network_ip.py
+++ challenge6/test_network_ip.py
@@ -14,34 +14,3 @@ is in a given network:
 (4) Convert the candidate address to an integer (same procedure as above)
 (5) Compare (3) and (4) to determine if ip is in network
 """
-
-# (1)
-net = input("Enter network using CIDR notation: ")
-candidate_address = input("Enter an IP address: ")
-
-net_address, length = net.split("/")
-length = int(length)
-print("Network Address:", net_address)
-print("Subnet Length:", length)
-print("IP Address:", candidate_address)
-
-# (2)
-mask = int("1" * length + "0" * (32 - length), 2)
-
-# (3)
-octets = net_address.split(".")
-net_ip = 0
-for n in range(4):
-    net_ip += int(octets[3 - n]) << (8 * n)
-
-# (4)
-octets = [int(octet) for octet in candidate_address.split(".")]
-candidate_ip = 0
-for n in range(4):
-    candidate_ip += octets[3 - n] << (8 * n)
-
-# (5)
-if candidate_ip & mask == net_ip:
-    print(f"{candidate_address} belongs to {net_address}/{length}")
-else:
-    print(f"{candidate_address} does NOT belong to {net_address}/{length}!")
