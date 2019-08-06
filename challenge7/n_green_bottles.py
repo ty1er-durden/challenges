4e7cc7f7a96586012a6b95ae515bdede0ececedc
loudsquelch <35978618+loudsquelch@users.noreply.github.com>
2019-08-06 23:31:37 +0100

    Challenge 7 uploaded

---
+++ challenge7/n_green_bottles.py
@@ -0,0 +1,32 @@
+"""
+Write a program to generate the lyrics to help me sing my kid to sleep with
+his favourite song.  Sometimes he likes to start at twenty green bottles,
+other nights he might want 10 pink bottles.  My wife suffers from dyscalculia
+so there are bonus points for avoiding numbers written as digits as in the
+example below:
+
+    Two green bottles sitting on the wall,
+    Two green bottles sitting on the wall,
+    And if one green bottle should accidentally fall,
+    There’ll be one green bottle sitting on the wall.
+
+    One green bottle sitting on the wall,
+    One green bottle sitting on the wall,
+    And if one green bottle should accidentally fall,
+    There’ll be zero green bottles sitting on the wall.
+
+HINTS
+    - Use a function to generate the lyrics
+    - Tackle a simple case first (3 green bottles)
+    - IF YOU WANT MORE OF A CHALLENGE
+        - Update your function to take arguments for the
+          number of bottles and their colour
+        - Make sure your bottles are pluralized correctly
+    - IF YOU WANT EVEN MORE OF A CHALLENGE
+        - Add a second function to convert digits to text
+          HINT use a dictionary for the conversion*
+
+*If you wanted to use really big numbers, take a look at 'number_to_words'
+in this module:
+https://pypi.python.org/pypi/inflect
+"""
